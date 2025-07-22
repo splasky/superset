393176087e618972a02d9abdbee569f9ea5336f7
hychang <hychang.1997.tw@gmail.com>
2025-07-22 15:19:11 +0800

    Add unit test case

--- tests/integration_tests/db_engine_specs/datastore_tests.py
+++ tests/integration_tests/db_engine_specs/datastore_tests.py
@@ -23,7 +23,7 @@ from sqlalchemy import column
 
 from superset.connectors.sqla.models import TableColumn
 from superset.db_engine_specs.base import BaseEngineSpec
-from superset.db_engine_specs.bigquery import BigQueryEngineSpec
+from superset.db_engine_specs.datastore import DatastoreEngineSpec
 from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
 from superset.sql.parse import Table
 from tests.integration_tests.base_tests import SupersetTestCase
@@ -42,10 +42,10 @@ def mock_engine_with_credentials(*args, **kwargs):
     yield engine_mock
 
 
-class TestBigQueryDbEngineSpec(SupersetTestCase):
-    def test_bigquery_sqla_column_label(self):
+class TestDatastoreDbEngineSpec(SupersetTestCase):
+    def test_datastore_sqla_column_label(self):
         """
-        DB Eng Specs (bigquery): Test column label
+        DB Eng Specs (datastore): Test column label
         """
         test_cases = {
             "Col": "Col",
@@ -54,12 +54,12 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
             "12345_col": "_12345_col_8d390",
         }
         for original, expected in test_cases.items():
-            actual = BigQueryEngineSpec.make_label_compatible(column(original).name)
+            actual = DatastoreEngineSpec.make_label_compatible(column(original).name)
             assert actual == expected
 
     def test_timegrain_expressions(self):
         """
-        DB Eng Specs (bigquery): Test time grain expressions
+        DB Eng Specs (datastore): Test time grain expressions
         """
         col = column("temporal")
         test_cases = {
@@ -70,14 +70,14 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         }
         for type_, expected in test_cases.items():
             col.type = type_
-            actual = BigQueryEngineSpec.get_timestamp_expr(
+            actual = DatastoreEngineSpec.get_timestamp_expr(
                 col=col, pdf=None, time_grain="PT1H"
             )
             assert str(actual) == expected
 
     def test_custom_minute_timegrain_expressions(self):
         """
-        DB Eng Specs (bigquery): Test time grain expressions
+        DB Eng Specs (datastore): Test time grain expressions
         """
         col = column("temporal")
         test_cases = {
@@ -93,17 +93,17 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         }
         for type_, expected in test_cases.items():
             col.type = type_
-            actual = BigQueryEngineSpec.get_timestamp_expr(
+            actual = DatastoreEngineSpec.get_timestamp_expr(
                 col=col, pdf=None, time_grain="PT5M"
             )
             assert str(actual) == expected
 
     def test_fetch_data(self):
         """
-        DB Eng Specs (bigquery): Test fetch data
+        DB Eng Specs (datastore): Test fetch data
         """
 
-        # Mock a google.cloud.bigquery.table.Row
+        # Mock a google.cloud.datastore.table.Row
         class Row:
             def __init__(self, value):
                 self._value = value
@@ -113,20 +113,20 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
 
         data1 = [(1, "foo")]
         with mock.patch.object(BaseEngineSpec, "fetch_data", return_value=data1):
-            result = BigQueryEngineSpec.fetch_data(None, 0)
+            result = DatastoreEngineSpec.fetch_data(None, 0)
         assert result == data1
 
         data2 = [Row(1), Row(2)]
         with mock.patch.object(BaseEngineSpec, "fetch_data", return_value=data2):
-            result = BigQueryEngineSpec.fetch_data(None, 0)
+            result = DatastoreEngineSpec.fetch_data(None, 0)
         assert result == [1, 2]
 
     @mock.patch.object(
-        BigQueryEngineSpec, "get_engine", side_effect=mock_engine_with_credentials
+        DatastoreEngineSpec, "get_engine", side_effect=mock_engine_with_credentials
     )
-    @mock.patch.object(BigQueryEngineSpec, "get_time_partition_column")
-    @mock.patch.object(BigQueryEngineSpec, "get_max_partition_id")
-    @mock.patch.object(BigQueryEngineSpec, "quote_table", return_value="`table_name`")
+    @mock.patch.object(DatastoreEngineSpec, "get_time_partition_column")
+    @mock.patch.object(DatastoreEngineSpec, "get_max_partition_id")
+    @mock.patch.object(DatastoreEngineSpec, "quote_table", return_value="`table_name`")
     def test_get_extra_table_metadata(
         self,
         mock_quote_table,
@@ -135,7 +135,7 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         mock_get_engine,
     ):
         """
-        DB Eng Specs (bigquery): Test extra table metadata
+        DB Eng Specs (datastore): Test extra table metadata
         """
         database = mock.Mock()
         sql = "SELECT * FROM `table_name`"
@@ -145,12 +145,12 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         # Test no indexes
         mock_get_time_partition_column.return_value = None
         mock_get_max_partition_id.return_value = None
-        result = BigQueryEngineSpec.get_extra_table_metadata(database, tbl)
+        result = DatastoreEngineSpec.get_extra_table_metadata(database, tbl)
         assert result == {}
 
         mock_get_time_partition_column.return_value = "ds"
         mock_get_max_partition_id.return_value = "19690101"
-        result = BigQueryEngineSpec.get_extra_table_metadata(database, tbl)
+        result = DatastoreEngineSpec.get_extra_table_metadata(database, tbl)
         print(result)
         assert result == {
             "indexes": [{"cols": ["ds"], "name": "partitioned", "type": "partitioned"}],
@@ -161,12 +161,12 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
             },
         }
 
-    @mock.patch("superset.db_engine_specs.bigquery.BigQueryEngineSpec.get_engine")
-    @mock.patch("superset.db_engine_specs.bigquery.pandas_gbq")
-    @mock.patch("superset.db_engine_specs.bigquery.service_account")
+    @mock.patch("superset.db_engine_specs.datastore.DatastoreEngineSpec.get_engine")
+    @mock.patch("superset.db_engine_specs.datastore.pandas_gbq")
+    @mock.patch("superset.db_engine_specs.datastore.service_account")
     def test_df_to_sql(self, mock_service_account, mock_pandas_gbq, mock_get_engine):
         """
-        DB Eng Specs (bigquery): Test DataFrame to SQL contract
+        DB Eng Specs (datastore): Test DataFrame to SQL contract
         """
         mock_service_account.Credentials.from_service_account_info = mock.MagicMock(
             return_value="account_info"
@@ -179,7 +179,7 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
 
         df = DataFrame()
         database = mock.MagicMock()
-        BigQueryEngineSpec.df_to_sql(
+        DatastoreEngineSpec.df_to_sql(
             database=database,
             table=Table(table="name", schema="schema"),
             df=df,
@@ -195,15 +195,15 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         )
 
     def test_extract_errors(self):
-        msg = "403 POST https://bigquery.googleapis.com/bigquery/v2/projects/test-keel-310804/jobs?prettyPrint=false: Access Denied: Project profound-keel-310804: User does not have bigquery.jobs.create permission in project profound-keel-310804"  # noqa: E501
-        result = BigQueryEngineSpec.extract_errors(Exception(msg))
+        msg = "403 POST https://datastore.googleapis.com/datastore/v2/projects/python-datastore-sqlalchemy/jobs?prettyPrint=false: Access Denied: Project python-datastore-sqlalchemy: User does not have datastore.jobs.create permission in project python-datastore-sqlalchemy"  # noqa: E501
+        result = DatastoreEngineSpec.extract_errors(Exception(msg))
         assert result == [
             SupersetError(
-                message='Unable to connect. Verify that the following roles are set on the service account: "BigQuery Data Viewer", "BigQuery Metadata Viewer", "BigQuery Job User" and the following permissions are set "bigquery.readsessions.create", "bigquery.readsessions.getData"',  # noqa: E501
+                message='Unable to connect. Verify that the following roles are set on the service account: "Datastore Data Viewer", "Datastore Metadata Viewer", "Datastore Job User" and the following permissions are set "datastore.readsessions.create", "datastore.readsessions.getData"',  # noqa: E501
                 error_type=SupersetErrorType.CONNECTION_DATABASE_PERMISSIONS_ERROR,
                 level=ErrorLevel.ERROR,
                 extra={
-                    "engine_name": "Google BigQuery",
+                    "engine_name": "Google Datastore",
                     "issue_codes": [
                         {
                             "code": 1017,
@@ -214,15 +214,15 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
             )
         ]
 
-        msg = "bigquery error: 404 Not found: Dataset fakeDataset:bogusSchema was not found in location"  # noqa: E501
-        result = BigQueryEngineSpec.extract_errors(Exception(msg))
+        msg = "datastore error: 404 Not found: Dataset fakeDataset:bogusSchema was not found in location"  # noqa: E501
+        result = DatastoreEngineSpec.extract_errors(Exception(msg))
         assert result == [
             SupersetError(
                 message='The schema "bogusSchema" does not exist. A valid schema must be used to run this query.',  # noqa: E501
                 error_type=SupersetErrorType.SCHEMA_DOES_NOT_EXIST_ERROR,
                 level=ErrorLevel.ERROR,
                 extra={
-                    "engine_name": "Google BigQuery",
+                    "engine_name": "Google Datastore",
                     "issue_codes": [
                         {
                             "code": 1003,
@@ -238,14 +238,14 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         ]
 
         msg = 'Table name "badtable" missing dataset while no default dataset is set in the request'  # noqa: E501
-        result = BigQueryEngineSpec.extract_errors(Exception(msg))
+        result = DatastoreEngineSpec.extract_errors(Exception(msg))
         assert result == [
             SupersetError(
                 message='The table "badtable" does not exist. A valid table must be used to run this query.',  # noqa: E501
                 error_type=SupersetErrorType.TABLE_DOES_NOT_EXIST_ERROR,
                 level=ErrorLevel.ERROR,
                 extra={
-                    "engine_name": "Google BigQuery",
+                    "engine_name": "Google Datastore",
                     "issue_codes": [
                         {
                             "code": 1003,
@@ -261,14 +261,14 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         ]
 
         msg = "Unrecognized name: badColumn at [1:8]"
-        result = BigQueryEngineSpec.extract_errors(Exception(msg))
+        result = DatastoreEngineSpec.extract_errors(Exception(msg))
         assert result == [
             SupersetError(
                 message='We can\'t seem to resolve column "badColumn" at line 1:8.',
                 error_type=SupersetErrorType.COLUMN_DOES_NOT_EXIST_ERROR,
                 level=ErrorLevel.ERROR,
                 extra={
-                    "engine_name": "Google BigQuery",
+                    "engine_name": "Google Datastore",
                     "issue_codes": [
                         {
                             "code": 1003,
@@ -284,14 +284,14 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
         ]
 
         msg = 'Syntax error: Expected end of input but got identifier "from_"'
-        result = BigQueryEngineSpec.extract_errors(Exception(msg))
+        result = DatastoreEngineSpec.extract_errors(Exception(msg))
         assert result == [
             SupersetError(
                 message='Please check your query for syntax errors at or near "from_". Then, try running your query again.',  # noqa: E501
                 error_type=SupersetErrorType.SYNTAX_ERROR,
                 level=ErrorLevel.ERROR,
                 extra={
-                    "engine_name": "Google BigQuery",
+                    "engine_name": "Google Datastore",
                     "issue_codes": [
                         {
                             "code": 1030,
@@ -302,8 +302,8 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
             )
         ]
 
-    @mock.patch("superset.models.core.Database.db_engine_spec", BigQueryEngineSpec)
-    @mock.patch("sqlalchemy_bigquery._helpers.create_bigquery_client", mock.Mock)
+    @mock.patch("superset.models.core.Database.db_engine_spec", DatastoreEngineSpec)
+    @mock.patch("sqlalchemy_datastore._helpers.create_datastore_client", mock.Mock)
     @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
     def test_calculated_column_in_order_by(self):
         table = self.get_table(name="birth_names")
@@ -319,7 +319,7 @@ class TestBigQueryDbEngineSpec(SupersetTestCase):
             """,
         )
 
-        table.database.sqlalchemy_uri = "bigquery://"
+        table.database.sqlalchemy_uri = "datastore://"
         query_obj = {
             "groupby": ["gender_cc"],
             "is_timeseries": False,
